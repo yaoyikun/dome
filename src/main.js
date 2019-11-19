6dbf5dca90770650eeaca17ec26a4754f935afe6
kun <971909506@qq.com>
2019-11-19 22:54:02 +0800

    整合elementUI

--- src/main.js
+++ src/main.js
@@ -1,12 +1,22 @@
 import Vue from 'vue'
 import App from './App.vue'
 
-//导入router
+
+//导入elementUi
+import ElementUI from 'element-ui'
+//导入element全局css样式
+import 'element-ui/lib/theme-chalk/index.css';
+//使用elementUi
+Vue.use(ElementUI);
+
+
+
+//导入router  路由管理
 import router from './router/router.js'
 
 Vue.config.productionTip = false
 
 new Vue({
-  router,
+  router,//路由实例 挂载 vue实例
   render: h => h(App),
 }).$mount('#app')
