c0144414334b9dc46b6005d7cc2728d572001cb5
kun <971909506@qq.com>
2019-11-19 23:05:49 +0800

    登录模块-路由整合

--- src/router/router.js
+++ src/router/router.js
@@ -8,10 +8,15 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 //2.使用路由
 Vue.use(VueRouter);
+
+//导入组件
+import login from '../views/login/login.vue'
 //3.创建路由规则
-const routes= [];
+const routes= [
+    {path:'/login', component:login}
+];
 //4.创建路由实例化对象  并挂载路由规则
-const router=new router({
+const router=new VueRouter({
     routes
 })
 //暴露出去,下一步实现路由实例挂载Vue实例
--- src/views/login/login.vue
+++ src/views/login/login.vue
@@ -1,5 +1,5 @@
 <template>
-  <router-view></router-view>
+  <h2>这是login组件页</h2>
 </template>
 
 <script>
